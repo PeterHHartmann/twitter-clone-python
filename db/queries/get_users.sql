033e6751c5709d3da484ca4f798e24ddab8ddf7a
Peter <peter.helle.hartmann@gmail.com>
2022-04-29 19:37:37 +0200

    reworked who_to_follow query again...

--- db/queries/get_users.sql
+++ db/queries/get_users.sql
@@ -1,49 +1,23 @@
 SELECT 
-    user_details.user_name, 
-    user_details.display_name,
-    profile_pictures.image_name AS pfp_image_name, 
-    banners.image_name AS banner_image_name,
-    users.user_email
-FROM user_details 
-JOIN profile_pictures ON profile_pictures.user_name = user_details.user_name
-JOIN banners ON banners.user_name = user_details.user_name
-JOIN users ON users.user_name = user_details.user_name
-LEFT JOIN email_validations ON email_validations.user_email = users.user_email
-WHERE NOT user_details.user_name = :user_name
-AND NOT user_details.user_name = "admin"
-AND email_validations.user_email IS NULL
-AND users.user_name IN (
-    SELECT DISTINCT u.user_name
-    FROM users u
-    LEFT OUTER JOIN follows f
-    ON f.user_name = u.user_name
-    WHERE NOT u.user_name = :user_name
-    AND NOT u.user_name IN (
-        SELECT ff.follows_user
-        FROM follows ff
-        LEFT OUTER JOIN users uu
-        ON ff.user_name = uu.user_name
-        WHERE uu.user_name = :user_name
-    )
+    u.user_name, 
+    ud.display_name,
+    pp.image_name AS pfp_image_name, 
+    b.image_name AS banner_image_name
+FROM users u, user_details ud, profile_pictures pp, banners b
+WHERE NOT ud.user_name = "Tom"
+AND NOT ud.user_name = "admin"
+AND ud.user_name = u.user_name
+AND pp.user_name = u.user_name
+AND b.user_name = u.user_name
+AND NOT u.user_name IN (
+    SELECT f.follows_user 
+    FROM follows f
+    WHERE f.user_name = "Tom"
+)
+AND NOT u.user_email IN (
+    SELECT e.user_email
+    FROM email_validations e
+    WHERE e.user_email = u.user_email
 )
 ORDER BY joined_date DESC 
 LIMIT 10;
-
-SELECT DISTINCT uu.user_name
-FROM users uu
-LEFT OUTER JOIN follows ff
-ON ff.user_name = uu.user_name
-WHERE NOT uu.user_name = "testaccount"
-AND NOT uu.user_name IN (
-    SELECT f.follows_user
-    FROM follows f
-    LEFT OUTER JOIN users u
-    ON f.user_name = u.user_name
-    WHERE u.user_name = "testaccount"
-);
-
-SELECT f.follows_user
-FROM follows f
-LEFT OUTER JOIN users u
-ON f.user_name = u.user_name
-WHERE u.user_name = "testaccount";
--- routes/home.py
+++ routes/home.py
@@ -11,7 +11,6 @@ def _():
 
     tweets_from_follows = db.tweets_get_following(session['user_name'])
     who_to_follow = db.details_get_who_to_follow(session['user_name'])
-    print(who_to_follow)
     profile_picture = db.profile_picture_get(session['user_name'])
 
     return dict(**session, tweets_from_follows=tweets_from_follows, who_to_follow=who_to_follow, profile_picture=profile_picture['image_name'])
